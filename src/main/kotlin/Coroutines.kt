b70c199a6a07ea1e9c0438b2e3293ba9864f5497
mvukic <matija.vukic2@gmail.com>
2021-02-18 21:19:48 +0100

    updates

--- src/main/kotlin/Coroutines.kt
+++ src/main/kotlin/Coroutines.kt
@@ -1,13 +1,32 @@
-import kotlinx.coroutines.future.await
-import java.net.http.HttpClient
+import kotlinx.coroutines.*
 
-suspend fun coroutines() {
-    val client: HttpClient = HttpClient.newBuilder().build()
+suspend fun coroutines() = coroutineScope {
+//    val client: HttpClient = HttpClient.newBuilder().build()
+//
+//    val url2Result = apiCall2(client).await()
+//    val url1Result = apiCall1(client).await()
+//
+//    println(url1Result)
+//    println(url2Result)
 
-    val url1Result = apiCall1(client).await() // 1 second
-    val url2Result = apiCall2(client).await() // 10 seconds
+    val result1 = async { delay2() }
+    val result2 = async { delay5() }
 
-    println(url1Result)
-    println(url2Result)
+    println(result1.await())
+    println(result2.await())
 
+}
+
+suspend fun delay2(): String {
+    println("Start: Call delay2()")
+    delay(2000)
+    println("End: Call delay2()")
+    return "Delay of 2s"
+}
+
+suspend fun delay5(): String {
+    println("Start: Call delay5()")
+    delay(5000)
+    println("End: Call delay5()")
+    return "Delay of 5s"
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,25 @@
 import kotlinx.coroutines.runBlocking
+import java.time.Duration
+import kotlin.system.measureTimeMillis
 
 fun main() {
-    runBlocking {
-        // try catch simulates GlobalExceptionHandler
-        try {
-            coroutines()
-        } catch (e: Exception) {
-            when (e) {
-                is Exception1 -> println("ApiCall 1 ${e.message}")
-                is Exception2 -> println("ApiCall 2 ${e.message}")
+    println("Start")
+    println()
+    val duration = measureTimeMillis {
+        runBlocking {
+            // try catch simulates GlobalExceptionHandler
+            try {
+                coroutines()
+            } catch (e: Exception) {
+                when (e) {
+                    is Exception1 -> println("ApiCall 1 ${e.message}")
+                    is Exception2 -> println("ApiCall 2 ${e.message}")
+                }
             }
         }
     }
+    println()
+    println("End $duration")
 //    try {
 //        completableFeature()
 //    } catch (e: Exception) {
